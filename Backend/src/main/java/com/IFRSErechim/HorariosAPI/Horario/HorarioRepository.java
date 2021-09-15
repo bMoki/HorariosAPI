c45926e3187cd726e29db2a28be77c253d521ec8
bMoki <breno26sonda@gmail.com>
2021-09-15 17:59:28 -0300

    Frontend iniciado

--- Backend/src/main/java/com/IFRSErechim/HorariosAPI/Horario/HorarioRepository.java
+++ Backend/src/main/java/com/IFRSErechim/HorariosAPI/Horario/HorarioRepository.java
@@ -1,8 +1,10 @@
 package com.IFRSErechim.HorariosAPI.Horario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HorarioRepository extends JpaRepository<Horario,Long> {
+
 }
